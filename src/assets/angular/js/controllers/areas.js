d9100967cb03d980e953ea85877ea536217f605d
Juan Alberto Jimenez <jualjiman@gmail.com>
2017-03-21 15:16:30 -0600

    Fixing redimentions to form fields.

--- src/assets/angular/js/controllers/areas.js
+++ src/assets/angular/js/controllers/areas.js
@@ -3,6 +3,9 @@ app.controller('AreaCtrl', [
     function($scope, Area){
         Area.get().$promise.then(function(response){
             $scope.areas = response.results;
+            setTimeout(function(){
+                $(".button-collapse").sideNav();
+            }, 200);
         });
     }
 ]);
